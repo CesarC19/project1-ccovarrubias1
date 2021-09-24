f4da77ad9007131189e91916ced7b353b3c72c00
Cesar Covarrubias <ccovarrubias1@student.gsu.edu>
2021-09-24 03:22:54 +0000

    added artist name, album cover, and preview of song

--- project1.py
+++ project1.py
@@ -1,8 +1,10 @@
 import flask
 import os
 import spotipy
+import random
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import find_dotenv, load_dotenv
+
 
 load_dotenv(find_dotenv())
 cid = os.getenv("SPOTIPY_CLIENT_ID")
@@ -14,15 +16,35 @@ sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 
 app = flask.Flask(__name__)
+#"Breaking Benjamin"
+artList = ["Linkin Park", "Avenged Sevenfold", "Foo Fighters"]
 
-lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+LPURL = 'https://open.spotify.com/artist/6XyY86QOPPrYVGvF9ch6wz?si=BPocwTzhSUWOKd3ZNX6vaA&dl_branch=1'
+A7XURL = 'https://open.spotify.com/artist/0nmQIMXWTXfhgOBdNzhGOs?si=jL0sKoZ_QV6fVcSxmIsbvg&dl_branch=1'
+FFURL = 'https://open.spotify.com/artist/7jy3rLJdDQY21OgRLCZ9sD?si=MxXrrzSmRz2oLpTuVHHIog&dl_branch=1'
+
+randomArtist = random.choice(artList)
+
+artistURL = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+
+if(randomArtist == "Linkin Park"):
+    artistURL = LPURL
+
+if(randomArtist == "Avenged Sevenfold"):
+    artistURL = A7XURL
+
+if(randomArtist == "Foo Fighters"):
+    artistURL = FFURL
+
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-results = spotify.artist_top_tracks(lz_uri)
+results = spotify.artist_top_tracks(artistURL)
 
 for track in results['tracks'][:1]:
     print('track    : ' + track['name'])
     trackName= track['name']
+    #print('artist   : ' + track['artist'])
+    #artistName= track['artists']
     print('audio    : ' + track['preview_url'])
     preUrl= track['preview_url']
     print('cover art: ' + track['album']['images'][0]['url'])
@@ -31,8 +53,9 @@ for track in results['tracks'][:1]:
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html")
-    return render_template("trackName")
+    return flask.render_template("index.html", trackName=trackName, randomArtist=randomArtist, preUrl=preUrl, coverArt=coverArt)
+
+
 
 app.run(
     debug=True
