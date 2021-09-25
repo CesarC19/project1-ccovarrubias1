5139ee7fb6d2347592797c56ade9fba63d0f7ec0
Cesar Covarrubias <ccovarrubias1@student.gsu.edu>
2021-09-25 01:56:46 +0000

    Final commit, failed to incorporate genius lyrics

--- project1.py
+++ project1.py
@@ -5,18 +5,17 @@ import random
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import find_dotenv, load_dotenv
 
-
 load_dotenv(find_dotenv())
 cid = os.getenv("SPOTIPY_CLIENT_ID")
 secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 BASE_URL = os.getenv("SPOTIPY_REDIRECT_URL")
+GAT = os.getenv("GENIUS_ACCESS_TOKEN")
 
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-
 app = flask.Flask(__name__)
-#"Breaking Benjamin"
+
 artList = ["Linkin Park", "Avenged Sevenfold", "Foo Fighters"]
 
 LPURL = 'https://open.spotify.com/artist/6XyY86QOPPrYVGvF9ch6wz?si=BPocwTzhSUWOKd3ZNX6vaA&dl_branch=1'
@@ -29,13 +28,10 @@ artistURL = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
 
 if(randomArtist == "Linkin Park"):
     artistURL = LPURL
-
 if(randomArtist == "Avenged Sevenfold"):
     artistURL = A7XURL
-
 if(randomArtist == "Foo Fighters"):
     artistURL = FFURL
-
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 results = spotify.artist_top_tracks(artistURL)
@@ -52,13 +48,9 @@ if len(items) > 0:
     print(artist['name'], artist['images'][0]['url'])
     artistImage= artist['images'][0]['url']
 
-
-
 @app.route("/")
 def index():
     return flask.render_template("index.html", trackName=trackName, randomArtist=randomArtist, artistImage=artistImage,  preUrl=preUrl, coverArt=coverArt)
-
-
 
 app.run(
     debug=True
